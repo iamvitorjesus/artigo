8205df43521ba454490be905b43d3b0c71f2e43d
iamvitorjesus <iamvitorjesus@github.com>
2020-06-22 00:30:45 -0300

    Mon-22-06-2020-00:30
    Mon-22-06-2020-00:30

--- app/static/js/own.js
+++ app/static/js/own.js
@@ -18,6 +18,7 @@ function change(id){
   captar = document.getElementById(id).style;
   percent = Number(captar.slice(7,9));
   x = 100/19;
+
   percent = percent + x;
   document.getElementById(id).style = percent;
   document.getElementById(id).innerHTML = "A barra de progresso está Funcionando"
@@ -25,28 +26,25 @@ function change(id){
 }
 
 function draw(bw, h, nbar, n, o, ot ){
-  function draw(bw, h, nbar, n, o, ot,c ){
-    var canv = document.getElementById("myCanvas");
-    var concrete = canv.getContext("2d");
-    var  rebar= canv.getContext("2d");
-    var cord1 = c+(ot/2)
-    var cord2 = bw-(c*2)-(ot)
-    var cord3 = h-(c*2)-(ot)
-    
-    concrete.fillStyle = "#d3d9df";
-    concrete.fillRect(0, 0, bw, h);
 
+  var canv = document.getElementById("myCanvas");
+  var concrete = canv.getContext("2d");
+  var rebar = canv.getContext("2d");
+  var cord1 = c+(ot/2);
+  var cord2 = bw-(c*2)-(ot);
+  var cord3 = h-(c*2)-(ot);
 
-    concrete.lineWidth = ot;
-    concrete.strokeRect(cord1, cord1, cord2 , cord3);
+  concrete.fillStyle = "#d3d9df";
+  concrete.fillRect(0, 0, bw, h);
 
+  concrete.lineWidth = ot;
+  concrete.strokeRect(cord1, cord1, cord2 , cord3);
 
-    rebar.strokeStyle = 'black';
-    rebar.fillStyle = 'black';
-    rebar.arc(bw/2,h/2,o/2,0,Math.PI*2);
-    rebar.stroke();
-    rebar.fill();
-  }
+  rebar.strokeStyle = 'black';
+  rebar.fillStyle = 'black';
+  rebar.arc(bw/2,h/2,o/2,0,Math.PI*2);
+  rebar.stroke();
+  rebar.fill();
 
   x = draw(200,500,6,2,10,5, 25)
 
