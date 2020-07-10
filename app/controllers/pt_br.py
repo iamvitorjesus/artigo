5933107b957d4bd85ae7ef029aea360299dfb538
iamvitorjesus <iamvitorjesus@github.com>
2020-07-10 15:22:17 -0300

    Fri-10-07-2020-15:22

--- app/controllers/pt_br.py
+++ app/controllers/pt_br.py
@@ -52,13 +52,61 @@ def novoprojeto():
             return redirect(url_for("resultados"))
         return render_template('pt/novoprojeto.html', logo6 = logo6)
 
-@app.route("/erroM", methods = ["GET"])
-def erroMomento():
-    return render_template('pt/erroMomento.html', logo6 = logo6)
 
-@app.route("/erroB", methods = ["GET"])
-def erroBiela():
-    return render_template('pt/erroBiela.html', logo6 = logo6)
+
+@app.route("/exemplo1", methods = ["POST", "GET"])
+def exemplo1():
+    if request.method == "POST":
+        dimen = {}
+        for info in request.form:           #retira informação dos inputs
+            value = request.form[info]
+            if value.isnumeric() == True:
+                value = float(value)
+            dimen[info] = value
+
+
+        Dic = conversao_unidades(dimen)
+        Dic = dimensionar(Dic)
+        if Dic['As'] + Dic['Ass'] >= 0.04*Dic['bw']*Dic['h']: #Verificação da Armadura Máxima
+            return redirect(url_for("erroMomento"))
+
+        if Dic['Vsd'] >= Dic['Vrd2']: # As bielas serão esmagadas.
+            #É necessário um redimencionamento ou aumento do fck''')
+            return redirect(url_for("erroBiela"))
+
+        session["dic"] = Dic
+        return redirect(url_for("resultados"))
+    else:
+        if "user" in session:
+            return redirect(url_for("resultados"))
+        return render_template('pt/exmplo1.html', logo6 = logo6)
+
+@app.route("/exemplo2", methods = ["POST", "GET"])
+def exemplo2():
+    if request.method == "POST":
+        dimen = {}
+        for info in request.form:           #retira informação dos inputs
+            value = request.form[info]
+            if value.isnumeric() == True:
+                value = float(value)
+            dimen[info] = value
+
+
+        Dic = conversao_unidades(dimen)
+        Dic = dimensionar(Dic)
+        if Dic['As'] + Dic['Ass'] >= 0.04*Dic['bw']*Dic['h']: #Verificação da Armadura Máxima
+            return redirect(url_for("erroMomento"))
+
+        if Dic['Vsd'] >= Dic['Vrd2']: # As bielas serão esmagadas.
+            #É necessário um redimencionamento ou aumento do fck''')
+            return redirect(url_for("erroBiela"))
+
+        session["dic"] = Dic
+        return redirect(url_for("resultados"))
+    else:
+        if "user" in session:
+            return redirect(url_for("resultados"))
+        return render_template('pt/exemplo2.html', logo6 = logo6)
 
 
 from viga.detalhamento_flexao import detalhamento_flexao
@@ -81,3 +129,12 @@ def resultados():
 @app.route("/contato")
 def contato():
     return render_template('pt/contato.html', logo6 = logo6)
+
+
+@app.route("/erroM", methods = ["GET"])
+def erroMomento():
+    return render_template('pt/erroMomento.html', logo6 = logo6)
+
+@app.route("/erroB", methods = ["GET"])
+def erroBiela():
+    return render_template('pt/erroBiela.html', logo6 = logo6)
