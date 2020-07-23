5be57bb25f60c6fc538bac9fa35922e8bc7e3f25
iamvitorjesus <iamvitorjesus@github.com>
2020-07-22 22:10:37 -0300

    Wed-22-07-2020-22:10 - Alteração no Host

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_script import Manager
+from flask_script import Server, Manager
 from flask_migrate import Migrate, MigrateCommand
 import os
 
@@ -15,7 +15,7 @@ db  = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 manager = Manager(app)
-manager.add_command("db", MigrateCommand)
+manager.add_command("runserver", Server(host='0.0.0.0', port = 8085))
 
 from app.models import tables
 from app.controllers import default
